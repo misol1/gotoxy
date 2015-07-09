38c4f37f021b088c4a2d8d337df4cdf4be64f311
misol1 <dal97msn@yahoo.com>
2015-07-10 00:58:56 +0200

    No need to copy existing text to buffer

--- sprite3.bat
+++ sprite3.bat
@@ -24,9 +24,9 @@ cmdwiz saveblock tempblock 0 %YP% 80 8
 for /F "tokens=*" %%i in (tempblock.gxy) do set BLOCK="%%i"
 
 :REP
-for /L %%a in (3,1,66) do gotoxy.exe 0 0 "\o0;%YP%;80;8%BLOCK:~1,-1%\p%%a;0%spi%%DELAY%" 0 0 r
+for /L %%a in (3,1,66) do gotoxy.exe 0 0 "\O0;%YP%;80;8%BLOCK:~1,-1%\p%%a;0%spi%%DELAY%" 0 0 r
 cmdwiz getch w&if !ERRORLEVEL! == 27 goto OUT
-for /L %%a in (66,-1,3) do gotoxy.exe 0 0 "\o0;%YP%;80;8%BLOCK:~1,-1%\p%%a;0%spi%%DELAY%" 0 0 r
+for /L %%a in (66,-1,3) do gotoxy.exe 0 0 "\O0;%YP%;80;8%BLOCK:~1,-1%\p%%a;0%spi%%DELAY%" 0 0 r
 cmdwiz getch w
 if not %ERRORLEVEL% == 27 goto REP
 
