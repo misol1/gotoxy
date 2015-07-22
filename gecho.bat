d365669ea3d9a62522fd4edb55b57207b69f9200
misol1 <dal97msn@yahoo.com>
2015-07-22 06:18:02 +0200

    Ignore control codes for gecho, unless 4th param given

--- gecho.bat
+++ gecho.bat
@@ -1,7 +1,8 @@
 ::Echo with color. Works best with single lines.
 @echo off
 set TEXT=""&if not "%~1" == "" set TEXT=%1
-set FG=u&if not "%~2" == "" set FG=%2
+set CC=+&if not "%~4" == "" set CC=
+set FG=%CC%u&if not "%~2" == "" set FG=%CC%%2
 set BG=U&if not "%~3" == "" set BG=%3
 gotoxy k k %TEXT% %FG% %BG% c w&echo.
-set TEXT=&set FG=&set BG=
+set TEXT=&set FG=&set BG=&set CC=
