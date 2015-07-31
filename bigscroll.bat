3240ce2c066220ef29ac6b78a275c3253c10c992
misol1 <dal97msn@yahoo.com>
2015-07-31 21:27:06 +0200

    Bouncing scroller

--- bigscroll.bat
+++ bigscroll.bat
@@ -12,6 +12,12 @@ cls
 
 call font3.bat
 
+call sintable.bat
+for /L %%a in (361,1,720) do set SIN%%a=
+set CNT=0&for /L %%a in (0,12,359) do set MSIN!CNT!=!SIN%%a!&set /A CNT+=1
+for /L %%a in (0,1,360) do set SIN%%a=
+set SC=0
+
 set /a YB=2
 set /a YB2=%YH%-%CHARH%-2-2
 
@@ -51,6 +57,8 @@ set OUT=""
 set /a PREPC=(%XPROG%-%XW%-%CHARW%)/%CHARW%
 if %PREPC% lss 1 set PREPC=0
 
+set /a YP=28-(!MSIN%SC%!*25^>^>14)
+
 :PREPLOOP
 set /a XP=%XW%+%PREPC%*%CHARW%-%XPROG%
 if %XP% geq %XW% goto BIGSKIP
@@ -67,9 +75,11 @@ gotoxy.exe %BXP% 0 "\O0;0;%XW%;%YH%%ANIM0:~1,-1%\vV%OUT:~1,-1%%DELAY%"
 set /a XPROG+=1
 if %XPROG% gtr %XMAX% set XPROG=%XW%
 
-set /a YP+=%YD%
-if %YP% lss %YB% set YD=1
-if %YP% gtr %YB2% set YD=-1
+::set /a YP+=%YD%
+::if %YP% lss %YB% set YD=1
+::if %YP% gtr %YB2% set YD=-1
+
+set /a SC+=1 & if !SC! geq 30 set /A SC=0
 
 set /a BXP+=1
 if %BXP% geq 0 set BXP=-20
