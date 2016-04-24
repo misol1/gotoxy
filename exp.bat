64927f5109dc890d5e4a23c4368acddca3335aa1
misol1 <dal97msn@yahoo.com>
2016-04-24 11:14:26 +0200

    New pattern in exp

--- exp.bat
+++ exp.bat
@@ -1,3 +1,4 @@
+@if not "%~1"=="" cls&gotoxy k k \f0\M52{\++\M28{\+0\g07\gfa\gdb\gb2\g20\gfe\}\n} e 0 xc&goto :eof
 gotoxy 0 k "\M16{\gdb\+0}" 0 0 x
 gotoxy 0 k "\M16{\M4{\gdb\}\+0}" 0 0 x
 gotoxy 0 k "\M4{\M16{\M5{\gdb\\}\+0\}\n}" 0 0 xcs
