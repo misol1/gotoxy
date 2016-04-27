358ff5dfa29bb2d4e6528674e9a6eda50b9a077b
misol1 <dal97msn@yahoo.com>
2016-04-27 17:46:34 +0200

    ..

--- ascii.bat
+++ ascii.bat
@@ -3,3 +3,4 @@ set TEXT=""&if not "%~1" == "" set TEXT="%~1"
 set COL=u&if not "%2" == "" set COL=%2
 if "%3"=="" gotoxy_extended k k %TEXT% %COL% 0 csF
 if not "%3"=="" gotoxy_extended 0 0 "%TEXT%" %COL% 0 F
+set TEXT=&set COL=
