9788758060047c87dcaecc5334d4df532b301943
misol1 <dal97msn@yahoo.com>
2016-05-03 17:10:40 +0200

    More/changed options for sequence.bat

--- sequence.bat
+++ sequence.bat
@@ -1,14 +1,15 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
-if "%~1" == "" echo Usage: sequence file.seq [x] [y] [color] [delay] [bcol] [norep]&goto :eof
+if "%~1" == "" echo Usage: sequence file.seq [delay] [x] [y] [color] [bcol] [normal|reverse|bounce] [norep]&goto :eof
 cmdwiz showcursor 0
 
-set COL=u&if not "%4"=="" set COL=%4
+set DELAY=10&if not "%2"=="" set DELAY=%2
+set XP=0&if not "%3"=="" set XP=%3
+set YP=0&if not "%4"=="" set YP=%4
+set COL=u&if not "%5"=="" set COL=%5
 set BCOL=U&if not "%6"=="" set BCOL=%6
-set REP=1&if not "%7"=="" set REP=2
-set DELAY=10&if not "%5"=="" set DELAY=%5
-set XP=0&if not "%2"=="" set XP=%2
-set YP=0&if not "%3"=="" set YP=%3
+set MODE=normal&if not "%7"=="" set MODE=%~7
+set REP=1&if not "%8"=="" set REP=2
 set FNAME=%~1
 
 if not exist "%~1" echo Error: file not found.&goto :OUTOF
@@ -16,12 +17,17 @@ set CNT=0&for /F "tokens=* delims=" %%a in (%~1) do set FNAME!CNT!="%%a"&set /A 
 set NOF=%CNT%&set CNT=0
 cls
 
+set DELTA=1&set ENDVAL=%NOF%
+if %MODE%==reverse set DELTA=-1&set ENDVAL=-1&set /A CNT=%NOF%-1
+
 :LOOP
 gotoxy %XP% %YP% !FNAME%CNT%! %COL% %BCOL% Fk
 if %ERRORLEVEL% == 27 goto OUTOF
+if %ERRORLEVEL% == 112 cmdwiz getch
 if %DELAY% gtr 0 cmdwiz delay %DELAY%
-set /A CNT+=1
-if %CNT% geq %NOF% set CNT=0&if %REP%==2 set REP=0
+set /A CNT+=%DELTA%
+if %CNT% == %NOF% set CNT=0&(if %MODE%==bounce set /A CNT=%NOF%-1&set DELTA=-1)&if %REP%==2 set REP=0
+if %CNT% == -1 set /A CNT=%NOF%-1&(if %MODE%==bounce set /A CNT=0&set DELTA=1)&if %REP%==2 set REP=0
 if %REP% gtr 0 goto LOOP
 
 :OUTOF
