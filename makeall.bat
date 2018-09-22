a96d2f30c91f3872439eec3d1e54bf0503bc2a77
misol101 <dal97msn@yahoo.com>
2018-09-22 17:08:20 +0100

    Copying same exes

--- makeall.bat
+++ makeall.bat
@@ -1,15 +1,19 @@
-gcc -o cmdwiz.exe cmdwizU.c -O2 -luser32 -lwinmm -lgdi32
+@windres rc\cmdwiz.rc rc\cmdwiz.o
+@windres rc\cmdwiz-ascii.rc rc\cmdwiz-ascii.o
+@windres rc\gotoxy-extended.rc rc\gotoxy-extended.o
+@windres rc\gotoxy.rc rc\gotoxy.o
+gcc -o cmdwiz.exe cmdwizU.c -O2 rc\cmdwiz.o -luser32 -lwinmm -lgdi32
 @if exist cmdwiz.exe strip cmdwiz.exe
 @echo.
-gcc -o cmdwiz-ascii.exe cmdwiz.c -O2 -luser32 -lwinmm -lgdi32
+gcc -o cmdwiz-ascii.exe cmdwiz.c -O2 rc\cmdwiz-ascii.o -luser32 -lwinmm -lgdi32
 @echo.
 gcc -o gotoxy.exe -O2 gotoxy.c
 @echo.
-gcc -DSUPPORT_EXTENDED -O2 -lshell32 -lwinmm -luser32 -o gotoxy_extended.exe gotoxy.c
+gcc -DSUPPORT_EXTENDED -O2 rc\gotoxy-extended.o -lshell32 -lwinmm -luser32 -o gotoxy_extended.exe gotoxy.c
 @echo.
-gcc -o gotoxyU.exe -O2 gotoxyU.c
+@rem gcc -o gotoxyU.exe -O2 gotoxyU.c
 @if exist cmdwiz.exe strip cmdwiz.exe
 @if exist cmdwiz-ascii.exe strip cmdwiz-ascii.exe
 @if exist gotoxy.exe strip gotoxy.exe
 @if exist gotoxy_extended.exe strip gotoxy_extended.exe
-@if exist gotoxyU.exe strip gotoxyU.exe
+@rem if exist gotoxyU.exe strip gotoxyU.exe
